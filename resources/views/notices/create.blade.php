7ea527fa8cbf2adeb4a480bfc1adcd932ea78ac0
abel <abelgoodwin1988@gmail.com>
2017-01-18 09:07:56 -0700

    Prepare Notice Requests

--- resources/views/notices/create.blade.php
+++ resources/views/notices/create.blade.php
@@ -33,4 +33,13 @@
             {!! Form::submit('Submit',['class'=>'btn btn-primary form-control']) !!}
         </div>
     {!! form::close() !!}
+
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    @endif
+
 @endsection
